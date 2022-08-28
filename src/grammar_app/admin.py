d24527ae6611c276805a8f2005982314eb33d5d8
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-08-28 13:05:14 -0300

    Adicionando os models #

--- src/grammar_app/admin.py
+++ src/grammar_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from grammar_app.models import Grammar, Grammar_Phrase
 # Register your models here.
+
+admin.site.register(Grammar_Phrase)
+admin.site.register(Grammar)
--- src/grammar_app/models.py
+++ src/grammar_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Grammar_Phrase(models.Model):
+    frase = models.CharField(max_length=200)
+    traducao = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.frase
+
+class Grammar(models.Model):
+    nivel_choices = (
+        ('N5','N5'),
+        ('N4','N4'),
+        ('N3','N3'),
+        ('N2','N2'),
+        ('N1','N1'),
+        ('UNKNOW','UNKNOW')
+    )
+
+    gramatica = models.CharField(max_length=200)
+    estrutura = models.CharField(max_length=200)
+    nivel = models.CharField(max_length=6, choices=nivel_choices)
+    frases = models.ForeignKey(Grammar_Phrase, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.gramatica
