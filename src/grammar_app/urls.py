7ab39786108776ee3c4de40d099878736bc2a549
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-09-03 10:35:27 -0300

    Corrigindo forms #1

--- src/grammar_app/urls.py
+++ src/grammar_app/urls.py
@@ -5,6 +5,9 @@ app_name = 'grammar'
 
 urlpatterns = [
     path('home/', views.index, name='home'),
+    path('phrases_list/', views.phrase_list, name='phrases_list'),
+    path('update_phrase/<int:pk>', views.phrase_update,name='update_phrase'),
+    path('delete_phrase/<int:pk>', views.phrase_delete, name='delete_phrase'),
     path('add/', views.grammar_create, name='add'),
     path('update/<int:pk>', views.grammar_update, name='update'),
     path('delete/<int:pk>', views.grammar_delete, name='delete'),
--- src/grammar_app/views.py
+++ src/grammar_app/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Grammar, Grammar_Phrase
 from .forms import GramarPhraseForm, GrammarForm
@@ -5,21 +6,37 @@ from .forms import GramarPhraseForm, GrammarForm
 
 def index(request):
     grammar = Grammar.objects.all()
-    grammar_phrase = Grammar_Phrase.objects.all()
     context = {
         'grammar': grammar,
-        'grammar_Phrase': grammar_phrase
     }
 
     return render(request, 'grammar_list.html', context)
 
+
+
+def phrase_list(request):
+    grammar_phrase = Grammar_Phrase.objects.all()
+    context = {
+        'grammar_phrase': grammar_phrase
+    }
+    return render(request, 'phrase_list.html', context)
+
+
+
 def grammar_create(request):
-    form_Grammar = GrammarForm(request.POST or None)
     form_phrase = GramarPhraseForm(request.POST or None)
-    if form_Grammar.is_valid() and form_phrase.is_valid():
-        form_Grammar.save()
-        form_phrase.save()
-        return redirect('grammar:home')
+    form_Grammar = GrammarForm(request.POST or None)
+    if request.method == 'POST':
+        if request.POST.get('phrase') == 'Salvar Frase': 
+            if form_phrase.is_valid():
+                form_phrase.save()
+                return redirect('grammar:home')
+
+        if request.POST.get('grammar') == 'Salvar Gramatica':
+           
+            if form_Grammar.is_valid():
+                form_Grammar.save()
+                return redirect('grammar:home')
     context = {
         'form_grammar': form_Grammar,
         'form_phrase': form_phrase
@@ -42,8 +59,29 @@ def grammar_update(request, pk):
     return render(request, 'grammar_form.html', context)
 
 
+def phrase_update(request,pk):
+    phrase = get_object_or_404(Grammar_Phrase, pk=pk)
+    form_phrase = GramarPhraseForm(request.POST or None, instance=phrase)
 
-def grammar_delete(request, id):
-    grammar = Grammar.objects.get(id = id)
+    if form_phrase.is_valid():
+        form_phrase.save()
+        return redirect('grammar:phrases_list')
+
+    context = {
+        "form_phrase": form_phrase
+    }
+
+    return render(request, 'grammar_form.html', context)
+
+
+
+def grammar_delete(request, pk):
+    grammar = Grammar.objects.get(id = pk)
     grammar.delete()
     return redirect('grammar:home')
+
+
+def phrase_delete(request, pk):
+    phrase = Grammar_Phrase.objects.get(id = pk)
+    phrase.delete()
+    return redirect('grammar:phrases_list')
