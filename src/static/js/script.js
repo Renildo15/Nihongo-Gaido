5ac2ed98b254b31034567a0ee7ea6bc7aaa78247
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-09-03 17:10:17 -0300

    Adicionando a pagina principal #2

---
+++ src/static/js/script.js
@@ -0,0 +1,11 @@
+var navContainer = document.getElementById("topnav");
+
+var links = navContainer.getElementsByClassName("link");
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className += " active";
+    });
+}
