f0fec7d7cc38997b37be908a27fad7485dc3d2ab
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-09-10 18:18:35 -0300

    Estilizando a tela de redifinir senha #6

--- src/user_app/urls.py
+++ src/user_app/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('logout/', views.deslogar_user, name="deslogar_user"),
     path('mudar_senha/', views.mudar_senha, name='mudar_senha'),
     path('mudar_senha_sucesso/', views.mudar_senha_sucesso, name='mudar_senha_sucesso'),
+    path("password_reset/", views.password_reset_request, name="password_reset"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="senha/password_reset_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="senha/password_reset_form.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="senha/password_reset_complete.html"), name='password_reset_complete'),
 ]
--- src/user_app/views.py
+++ src/user_app/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm, PasswordChangeForm, PasswordResetForm
 from .forms import RegisterUserForm, PasswordChangingForm
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.contrib.auth.models import User
+from django.core.mail import send_mail, BadHeaderError
+from django.template.loader import render_to_string
 
 
 def logar_user(request):
@@ -72,3 +79,32 @@ def mudar_senha(request):
 @login_required(login_url='user:logar_user')
 def mudar_senha_sucesso(request):
     return render(request, 'mudar_senha_sucesso.html')
+
+
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "senha/password_reset_email.txt"
+                    c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8001',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return redirect ("/auth/reset_password_sent/")
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="senha/password_reset.html", context={"password_reset_form":password_reset_form})
