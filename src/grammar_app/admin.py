92b670bd0ff84eef0c3075e8b8d3baa4d0b8c1cd
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-09-23 17:39:46 -0300

    Adicioando permissão personalizada por usuário no admin #22

--- src/grammar_app/admin.py
+++ src/grammar_app/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from grammar_app.models import Grammar
 # Register your models here.
-admin.site.register(Grammar)
+
+class GrammarAdmin(admin.ModelAdmin):
+    readonly_fields = ('criado_por', )
+
+    def save_model(self, request, obj, form, change):
+        usuario = request.user
+        obj.criado_por = usuario
+        super(GrammarAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(GrammarAdmin, self).get_queryset(request)
+        qs = qs.filter(criado_por=request.user)
+        return qs
+
+admin.site.register(Grammar, GrammarAdmin)
--- src/grammar_app/models.py
+++ src/grammar_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Grammar(models.Model):
@@ -14,5 +15,11 @@ class Grammar(models.Model):
     gramatica = models.CharField(max_length=200)
     estrutura = models.CharField(max_length=200)
     nivel = models.CharField(max_length=6, choices=nivel_choices)
+    criado_por = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "gramáticas"
+        ordering = ('gramatica', )
+
     def __str__(self):
         return self.gramatica
