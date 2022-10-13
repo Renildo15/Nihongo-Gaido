98e7412d61b483f6583b9e6ec7bf1081ad8f3ecf
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-10-13 17:18:08 -0300

    correção configuração media url #13

--- src/nihongo_gaido/urls.py
+++ src/nihongo_gaido/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document__root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
