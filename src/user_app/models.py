5c44187e33b046ca953a74912cf9948367fe3e06
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-10-13 16:19:05 -0300

    Adicionando página de perfil #13

--- src/user_app/models.py
+++ src/user_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
-    
-
-    
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    telefone = models.CharField(null=True, blank=True, max_length=15)
+    date_created = models.DateTimeField(auto_now_add = True, null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.user.name
