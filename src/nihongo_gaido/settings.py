f81d20706da820e8fd8df72c8918d31f1e591bcd
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-11-05 18:46:32 -0300

    Fazendo as migrations #43

--- src/nihongo_gaido/settings.py
+++ src/nihongo_gaido/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'grammar_app',
     'nihongo_pages_app',
     'phrases_app',
--- src/text_app/admin.py
+++ src/text_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Text, TextTraducao, TextWriting
 # Register your models here.
+admin.site.register(Text)
+admin.site.register(TextTraducao)
+admin.site.register(TextWriting)
