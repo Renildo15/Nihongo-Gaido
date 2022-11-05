4031b83f89259793f02de6d229a883f9cee1b628
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-11-05 20:37:03 -0300

    Adicionando views #43

--- src/text_app/urls.py
+++ src/text_app/urls.py
@@ -4,5 +4,6 @@ from text_app import views
 app_name = "text"
 
 urlpatterns = [
+    path('text_list/', views.text_list, name="text_list"),
     path("add_text/", views.text_create, name="add_text")
 ]
--- src/text_app/views.py
+++ src/text_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .forms import TextForm,TextTraducaoForm,TextWriting
+from .models import Text,TextTraducao, TextWriting
 # Create your views here.
+
+
+def text_list(request):
+    texts = Text.objects.filter(criado_por=request.user.id)
+    context = {
+        "texts": texts
+    }
+    return render(request, 'text_list.html', context)
+
 
 def text_create(request):
     text_form = TextForm
