cb87dbc43e015ccd624b6115239f9483761894ad
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-11-05 19:10:45 -0300

    Corrigindo as migrations #43

--- src/text_app/models.py
+++ src/text_app/models.py
@@ -30,7 +30,6 @@ class TextTraducao(models.Model):
     titulo_traducao = models.CharField(max_length=200, unique=True)
     texto_traducao = RichTextField()
     slug = models.SlugField(unique=True, blank=True, null=True)
-    date_created = models.DateTimeField(auto_now_add = True, null=True, blank=True)
     text_id = models.ForeignKey(Text,on_delete=models.CASCADE,blank=True, null=True)
     criado_por = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
 
