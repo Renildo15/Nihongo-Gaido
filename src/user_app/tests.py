73d0897b0876467a6b3dab49a35d4e2e87f990c4
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-11-24 19:07:25 -0300

    Add testes parte-o1 #43

--- src/user_app/tests.py
+++ src/user_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'
+        }
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        response = self.client.post("login/", self.credentials, follow=True)
+        print(response)
+
