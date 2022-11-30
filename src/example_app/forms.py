9d1fdb89be5af3d81f4ec54c503e00cc01683e46
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-11-30 19:21:10 -0300

    Corrigindo forms #58

--- src/example_app/forms.py
+++ src/example_app/forms.py
@@ -5,4 +5,13 @@ from .models import Example
 class ExampleForm(forms.ModelForm):
     class Meta:
         model = Example
-        fields = '__all__'
+        fields = ('frase', 'traducao', 'anotacao')
+        labels = {
+            'frase': 'Frase: ',
+            'traducao': 'Tradução: ',
+            'anotacao': 'Anotações'
+        }
+        exclude = ['slug','criado_por', 'palavra','leitura']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['anotacao'].widget = forms.Textarea(attrs={'style':'width:100%; border-radius:10px; padding:10px;'})
