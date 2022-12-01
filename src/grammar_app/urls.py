b1e617b77dda4cf51e90026b9aa62fef4e1787df
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-12-01 16:57:49 -0300

    Corrigindo bug dos grammar_app #62

--- src/grammar_app/urls.py
+++ src/grammar_app/urls.py
@@ -6,6 +6,6 @@ app_name = 'grammar'
 urlpatterns = [
     path('grammar_list/', views.grammar_list, name='grammar_list'),
     path('add_grammar/', views.grammar_create, name='add_grammar'),
-    path('update/<int:pk>', views.grammar_update, name='update'),
-    path('delete/<int:pk>', views.grammar_delete, name='delete'),
+    path('edit_grammar/<int:pk>', views.grammar_update, name='grammar_edit'),
+    path('delete_grammar/<int:pk>', views.grammar_delete, name='grammar_delete'),
 ]
