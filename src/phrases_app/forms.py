dd0f66591b1796efe7711efa10d01c035520f5bb
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-25 20:49:01 -0300

    refactor: substituindo o explicaçao por observação #71

--- src/phrases_app/forms.py
+++ src/phrases_app/forms.py
@@ -4,20 +4,19 @@ from .models import Grammar_Phrase
 class GramarPhraseForm(forms.ModelForm):
     class Meta:
         model = Grammar_Phrase
-        fields = ('frase', 'traducao','explicacao', 'grammar_id')
+        fields = ('frase', 'traducao','observacao', 'grammar_id')
         labels = {
             'frase': 'Frase: ',
             'traducao': 'Tradução: ',
-            'explicacao': 'Explicação: ',
+            'observacao': 'Observação: ',
         }
         
         widgets = {
             'grammar_id': forms.HiddenInput()
         }
-     
+
 
     def __init__(self, *args, **kwargs):
         super(GramarPhraseForm, self).__init__(*args, **kwargs)    
-        #self.fields['grammar_id'].disabled = True
         self.fields['grammar_id'].empty_label = "Selecione"
         self.fields['grammar_id'].label = ''
